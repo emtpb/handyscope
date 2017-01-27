0da42e73d925cb06590c0eb0e09386a7acce7d88
Tim Hetkaemper <timh1@mail.upb.de>
2017-01-27 13:48:48 +0100

    rename id to instr_id

--- tiepie/deviceList.py
+++ tiepie/deviceList.py
@@ -39,12 +39,12 @@ class DeviceList:
         """
         return libtiepie.LstGetCount()
 
-    def get_device_name(self, id, id_kind="index"):
+    def get_device_name(self, instr_id, id_kind="index"):
         """Get the full name of the device.
 
         Args:
-            id (int): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
-            id_kind (str): the kind of the given id (listed in dict ID_KINDS), defaults to device list index
+            instr_id (int): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
+            id_kind (str): the kind of the given instr_id (listed in dict ID_KINDS), defaults to device list index
 
         Returns:
             str: full device name
@@ -53,25 +53,25 @@ class DeviceList:
         id_kind_int = self.ID_KINDS[id_kind]
 
         # get length of device name string
-        str_len = libtiepie.LstDevGetName(id_kind_int, id, None, 0)
+        str_len = libtiepie.LstDevGetName(id_kind_int, instr_id, None, 0)
 
         # initialize mutable string buffer
         str_buffer = ctypes.create_string_buffer(str_len)
 
         # write the actual device name to the buffer
-        libtiepie.LstDevGetName(id_kind_int, id, str_buffer, str_len)
+        libtiepie.LstDevGetName(id_kind_int, instr_id, str_buffer, str_len)
 
         # convert to a normal python string
         dev_name = str_buffer.value.decode('utf-8')
 
         return dev_name
 
-    def get_device_name_short(self, id, id_kind="index"):
+    def get_device_name_short(self, instr_id, id_kind="index"):
         """Get the short name of the device.
 
         Args:
-            id (int): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
-            id_kind (str): the kind of the given id (listed in dict ID_KINDS), defaults to device list index
+            instr_id (int): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
+            id_kind (str): the kind of the given instr_id (listed in dict ID_KINDS), defaults to device list index
 
         Returns:
             str: short device name
@@ -80,25 +80,25 @@ class DeviceList:
         id_kind_int = self.ID_KINDS[id_kind]
 
         # get length of device name string
-        str_len = libtiepie.LstDevGetNameShort(id_kind_int, id, None, 0)
+        str_len = libtiepie.LstDevGetNameShort(id_kind_int, instr_id, None, 0)
 
         # initialize mutable string buffer
         str_buffer = ctypes.create_string_buffer(str_len)
 
         # write the actual device name to the buffer
-        libtiepie.LstDevGetNameShort(id_kind_int, id, str_buffer, str_len)
+        libtiepie.LstDevGetNameShort(id_kind_int, instr_id, str_buffer, str_len)
 
         # convert to a normal python string
         dev_name = str_buffer.value.decode('utf-8')
 
         return dev_name
 
-    def get_device_name_shortest(self, id, id_kind="index"):
+    def get_device_name_shortest(self, instr_id, id_kind="index"):
         """Get the shortest name of the device.
 
         Args:
-            id (int): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
-            id_kind (str): the kind of the given id (listed in dict ID_KINDS), defaults to device list index
+            instr_id (int): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
+            id_kind (str): the kind of the given instr_id (listed in dict ID_KINDS), defaults to device list index
 
         Returns:
             str: shortest device name
@@ -107,25 +107,25 @@ class DeviceList:
         id_kind_int = self.ID_KINDS[id_kind]
 
         # get length of device name string
-        str_len = libtiepie.LstDevGetNameShortest(id_kind_int, id, None, 0)
+        str_len = libtiepie.LstDevGetNameShortest(id_kind_int, instr_id, None, 0)
 
         # initialize mutable string buffer
         str_buffer = ctypes.create_string_buffer(str_len)
 
         # write the actual device name to the buffer
-        libtiepie.LstDevGetNameShortest(id_kind_int, id, str_buffer, str_len)
+        libtiepie.LstDevGetNameShortest(id_kind_int, instr_id, str_buffer, str_len)
 
         # convert to a normal python string
         dev_name = str_buffer.value.decode('utf-8')
 
         return dev_name
 
-    def get_device_serial_no(self, id, id_kind="index"):
+    def get_device_serial_no(self, instr_id, id_kind="index"):
         """Get the serial number of the device.
 
         Args:
-            id (int): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
-            id_kind (str): the kind of the given id (listed in dict ID_KINDS), defaults to device list index
+            instr_id (int): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
+            id_kind (str): the kind of the given instr_id (listed in dict ID_KINDS), defaults to device list index
 
         Returns:
             int: serial number
@@ -134,15 +134,15 @@ class DeviceList:
         id_kind_int = self.ID_KINDS[id_kind]
 
         # get the serial number
-        serial_no = libtiepie.LstDevGetSerialNumber(id_kind_int, id)
+        serial_no = libtiepie.LstDevGetSerialNumber(id_kind_int, instr_id)
 
         return serial_no
 
-    def get_device_types(self, id, id_kind="index"):
+    def get_device_types(self, instr_id, id_kind="index"):
         """Get the the device types of an instrument.
 
         Args:
-            id (int): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
+            instr_id (int): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
             id_kind (str): the kind of the given id (listed in dict ID_KINDS), defaults to device list index
 
         Returns:
@@ -152,7 +152,7 @@ class DeviceList:
         id_kind_int = self.ID_KINDS[id_kind]
 
         # get the device types
-        dev_types = libtiepie.LstDevGetTypes(id_kind_int, id)
+        dev_types = libtiepie.LstDevGetTypes(id_kind_int, instr_id)
 
         # check for every possible type and store in dict
         type_dict = {"Osc": False,
@@ -177,12 +177,12 @@ class DeviceList:
 
         return dev_list
 
-    def _open_device(self, id, id_kind="index", device_type="Osc"):
+    def _open_device(self, instr_id, id_kind="index", device_type="Osc"):
         """Open a device (of device_type) of an instrument (with given id) and return the device handle.
 
         Args:
-            id (int): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
-            id_kind (str): the kind of the given id (listed in dict ID_KINDS), defaults to device list index
+            instr_id (int): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
+            id_kind (str): the kind of the given instr_id (listed in dict ID_KINDS), defaults to device list index
             device_type (str): the type of the device (listed in dict DEVICE_TYPES), defaults to oscilloscope
 
         Returns:
@@ -192,4 +192,4 @@ class DeviceList:
         id_kind_int = self.ID_KINDS[id_kind]
         device_type_int = self.DEVICE_TYPES[device_type]
 
-        return libtiepie.LstOpenDevice(id_kind_int, id, device_type_int)
+        return libtiepie.LstOpenDevice(id_kind_int, instr_id, device_type_int)
