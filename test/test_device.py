d846607dce266526f5deffeb6426ca333fc96205
Tim Hetkaemper <timh1@mail.upb.de>
2017-02-01 17:56:31 +0100

    implement device functionality and test it
    test is currently hardcoded to a HS5 oscilloscope

---
+++ test/test_device.py
@@ -0,0 +1,70 @@
+
+
+class TestDevice:
+    def setup_class(self):
+        from tiepie.device import Device
+        self.device = Device("HS5", "product id", "Osc")
+
+    def test_driver_ver(self):
+        ver_list = self.device.driver_ver.split('.')
+        assert len(ver_list) == 4
+        for element in ver_list:
+            assert element.isdigit()
+
+    def test_firmware_ver(self):
+        ver_list = self.device.firmware_ver.split('.')
+        assert len(ver_list) == 4
+        for element in ver_list:
+            assert element.isdigit()
+
+    def test_calibration_date(self):
+        import datetime
+
+        assert type(self.device.calibration_date) is datetime.date
+
+    def test_serial_no(self):
+        assert type(self.device.serial_no) is int
+        assert self.device.serial_no >= 0
+
+    def test_product_id(self):
+        from tiepie.deviceList import DeviceList
+
+        product_id = self.device.product_id
+        assert type(product_id) is str
+        # Check if it is a valid key
+        assert product_id in DeviceList.PRODUCT_IDS
+
+    def test_device_type(self):
+        from tiepie.deviceList import DeviceList
+
+        device_type = self.device.device_type
+        assert type(device_type) is str
+        # Check if it is a valid key
+        assert device_type in DeviceList.DEVICE_TYPES
+
+    def test_short_name(self):
+        from tiepie.deviceList import DeviceList
+
+        name = self.device.short_name
+        assert type(name) is str
+        # Short name corresponds to product names -> look it up in the dict
+        assert name in DeviceList.PRODUCT_IDS
+
+    def test_name(self):
+        name = self.device.name
+        name_list = name.split('-')
+        assert len(name_list) == 2
+        # first part of name is the short name
+        assert name_list[0] == self.device.short_name
+
+    def test_long_name(self):
+        name = self.device.long_name
+        name_list = name.split(' ')
+        assert len(name_list) == 2
+        # first part of long name is always the manufacturer name
+        assert name_list[0] == "Handyscope"
+        # second part of long name is the name
+        assert name_list[1] == self.device.name
+
+    def test_is_removed(self):
+        assert self.device.is_removed is False
--- tiepie/device.py
+++ tiepie/device.py
@@ -1,74 +1,143 @@
 from tiepie.library import libtiepie
+from tiepie.deviceList import device_list
+from datetime import date
+import ctypes
 
 
 class Device:
-    def __init__(self, dev_handle):
-        self._dev_handle = dev_handle
+    def __init__(self, instr_id, id_kind, device_type):
+        self._dev_handle = device_list.open_device(instr_id, id_kind, device_type)
 
     def __del__(self):
         libtiepie.DevClose(self._dev_handle)
 
     @property
     def driver_ver(self):
-        return libtiepie.DevGetDriverVersion()
+        raw_version = libtiepie.DevGetDriverVersion(self._dev_handle)
+        return version_to_str(raw_version)
 
     @property
     def firmware_ver(self):
-        return libtiepie.DevGetFirmwareVersion()
+        raw_version = libtiepie.DevGetFirmwareVersion(self._dev_handle)
+        return version_to_str(raw_version)
 
     @property
     def calibration_date(self):
-        return libtiepie.DevGetCalibrationDate()
+        raw_date = libtiepie.DevGetCalibrationDate(self._dev_handle)
+        split_date = date(raw_date >> 16, (raw_date >> 8) & 0xff, raw_date & 0xff)
+        return split_date
 
     @property
     def serial_no(self):
-        return libtiepie.DevGetSerialVersion()
+        return libtiepie.DevGetSerialNumber(self._dev_handle)
 
     @property
     def product_id(self):
-        return libtiepie.DevGetProductId()
+        id_int = libtiepie.DevGetProductId(self._dev_handle)
+
+        # Lookup id in the dict
+        for key in device_list.PRODUCT_IDS:
+            if device_list.PRODUCT_IDS[key] == id_int:
+                # Return human readable string, i.e. the key name
+                return key
+
+        # If code hasn't returned yet, product id wasn't found
+        raise ValueError("Unknown product id: %s" % id_int)
 
     @property
     def device_type(self):
-        return libtiepie.DevGetType()
+        type_int = libtiepie.DevGetType(self._dev_handle)
+
+        # Lookup type in the dict
+        for key in device_list.DEVICE_TYPES:
+            if device_list.DEVICE_TYPES[key] == type_int:
+                # Return human readable string, i.e. the key name
+                return key
+
+        # If code hasn't returned yet, device type wasn't found
+        raise ValueError("Unknown device type: %s" % type_int)
 
     @property
     def long_name(self):
-        return libtiepie.DevGetName()
+        # get length of device name string
+        str_len = libtiepie.DevGetName(self._dev_handle, None, 0)
+
+        # initialize mutable string buffer
+        str_buffer = ctypes.create_string_buffer(str_len)
+
+        # write the actual device name to the buffer
+        libtiepie.DevGetName(self._dev_handle, str_buffer, str_len)
+
+        # convert to a normal python string
+        dev_name = str_buffer.value.decode('utf-8')
+
+        return dev_name
 
     @property
     def name(self):
-        return libtiepie.DevGetNameShort()
+        # get length of device name string
+        str_len = libtiepie.DevGetNameShort(self._dev_handle, None, 0)
+
+        # initialize mutable string buffer
+        str_buffer = ctypes.create_string_buffer(str_len)
+
+        # write the actual device name to the buffer
+        libtiepie.DevGetNameShort(self._dev_handle, str_buffer, str_len)
+
+        # convert to a normal python string
+        dev_name = str_buffer.value.decode('utf-8')
+
+        return dev_name
 
     @property
     def short_name(self):
-        return libtiepie.DevGetNameShortest()
+        # get length of device name string
+        str_len = libtiepie.DevGetNameShortest(self._dev_handle, None, 0)
+
+        # initialize mutable string buffer
+        str_buffer = ctypes.create_string_buffer(str_len)
+
+        # write the actual device name to the buffer
+        libtiepie.DevGetNameShortest(self._dev_handle, str_buffer, str_len)
+
+        # convert to a normal python string
+        dev_name = str_buffer.value.decode('utf-8')
+
+        return dev_name
 
     @property
     def is_removed(self):
-        return libtiepie.DevIsRemoved()
+        return libtiepie.DevIsRemoved(self._dev_handle) == 1
 
     def dev_close(self):
-        libtiepie.DevClose()
+        libtiepie.DevClose(self._dev_handle)
 
     @property
     def trig_ins(self):
+        # TODO implement class instantiation
         return None
 
     @property
     def trig_in_cnt(self):
-        return libtiepie.DevTrGetInputCount()
+        return libtiepie.DevTrGetInputCount(self._dev_handle)
 
-    def trig_in_by_id(self):
-        libtiepie.DevTrGetInputIndexById()
+    def trig_in_by_id(self, trig_in_id):
+        # TODO implement trig_in_id dict
+        libtiepie.DevTrGetInputIndexById(self._dev_handle, trig_in_id)
 
     @property
     def trig_outs(self):
+        # TODO implement class instantiation
         return None
 
     @property
     def trig_out_cnt(self):
-        return libtiepie.DevTrGetOutputCount()
+        return libtiepie.DevTrGetOutputCount(self._dev_handle)
 
-    def trig_out_by_id(self):
-        libtiepie.DevTrGetOutputIndexById()
+    def trig_out_by_id(self, trig_out_id):
+        # TODO implement trig_out_id dict
+        libtiepie.DevTrGetOutputIndexById(self._dev_handle, trig_out_id)
+
+
+def version_to_str(raw_version):
+    return '.'.join([str((raw_version >> (idx * 16)) & 0xffff) for idx in range(3,-1,-1)])
