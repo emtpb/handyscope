126dea07c1e57c3e55ede7a31cd6cd6567718352
Tim Hetkaemper <timh1@mail.uni-paderborn.de>
2017-04-24 13:51:21 +0200

    device: Add and format docstrings.

--- tiepie/device.py
+++ tiepie/device.py
@@ -7,13 +7,14 @@ import ctypes
 
 
 class Device:
-    """
-    Base class with functions common to all devices of an instrument, like name and serial number.
+    """Base class for devices.
+
+    This class contains methods common to all devices of an instrument, like name and serial number. A device is e.g. an
+    oscilloscope or a generator, whereas an instrument is e.g. the whole 'HS5'.
     """
 
     def __init__(self, instr_id, id_kind, device_type):
-        """
-        Constructor for class Device.
+        """Constructor for class Device.
 
         Args:
             instr_id (int or str): Device list index, product ID (listed in dict PRODUCT_IDS) or serial number
@@ -32,8 +33,7 @@ class Device:
 
     @property
     def driver_ver(self):
-        """
-        Get the driver version in the format Major.Minor.Release.Build
+        """Get the driver version in the format Major.Minor.Release.Build
 
         Returns:
             str: driver version in the format Major.Minor.Release.Build
@@ -43,8 +43,7 @@ class Device:
 
     @property
     def firmware_ver(self):
-        """
-        Get the firmware version in the format Major.Minor.Release.Build
+        """Get the firmware version in the format Major.Minor.Release.Build
 
         Returns:
             str: firmware version in the format Major.Minor.Release.Build
@@ -54,8 +53,7 @@ class Device:
 
     @property
     def calibration_date(self):
-        """
-        Get the calibration date
+        """Get the calibration date
 
         Returns:
             :py:class:`datetime.date`: calibration date
@@ -66,8 +64,7 @@ class Device:
 
     @property
     def serial_no(self):
-        """
-        Get the serial number
+        """Get the serial number
 
         Returns:
             int: serial number
@@ -76,8 +73,7 @@ class Device:
 
     @property
     def product_id(self):
-        """
-        Get the product id as human readable string (key of :py:attr:`tiepie.deviceList.DeviceList.PRODUCT_IDS`)
+        """Get the product id as human readable string (key of :py:attr:`tiepie.deviceList.DeviceList.PRODUCT_IDS`)
 
         Returns:
             str: product id
@@ -95,6 +91,11 @@ class Device:
 
     @property
     def device_type(self):
+        """Get the device type as human readable string (key of :py:attr:`tiepie.deviceList.DeviceList.DEVICE_TYPES`)
+
+        Returns:
+            str: device type
+        """
         type_int = libtiepie.DevGetType(self._dev_handle)
 
         # Lookup type in the dict
@@ -108,6 +109,11 @@ class Device:
 
     @property
     def long_name(self):
+        """Get the long name of the device.
+
+        Returns:
+            str: long device name (e.g. "Handyscope HS5-530XMS")
+        """
         # get length of device name string
         str_len = libtiepie.DevGetName(self._dev_handle, None, 0)
 
@@ -124,6 +130,11 @@ class Device:
 
     @property
     def name(self):
+        """Get the name of the device.
+
+        Returns:
+            str: device name (e.g. "HS5-530XMS")
+        """
         # get length of device name string
         str_len = libtiepie.DevGetNameShort(self._dev_handle, None, 0)
 
@@ -140,6 +151,11 @@ class Device:
 
     @property
     def short_name(self):
+        """Get the short name of the device.
+
+        Returns:
+            str: short device name (e.g. "HS5")
+        """
         # get length of device name string
         str_len = libtiepie.DevGetNameShortest(self._dev_handle, None, 0)
 
@@ -156,17 +172,34 @@ class Device:
 
     @property
     def is_removed(self):
+        """Check if device is removed.
+
+        Returns:
+            bool: True if removed, False otherwise.
+        """
         return libtiepie.DevIsRemoved(self._dev_handle) == 1
 
     def dev_close(self):
+        """Close the device.
+        """
         libtiepie.DevClose(self._dev_handle)
 
     @property
     def trig_ins(self):
+        """List of all trigger inputs.
+
+        Returns:
+            list: list of :py:class:`tiepie.triggerInput.TriggerInput`
+        """
         return self._trig_ins
 
     @property
     def trig_in_cnt(self):
+        """Count of available trigger inputs.
+
+        Returns:
+            int: count of trigger inputs
+        """
         try:
             return libtiepie.DevTrGetInputCount(self._dev_handle)
         except OSError as err:
@@ -183,10 +216,20 @@ class Device:
 
     @property
     def trig_outs(self):
+        """List of available trigger outputs.
+
+        Returns:
+            list: list of :py:class:`tiepie.triggerOutput.TriggerOutput`
+        """
         return self._trig_outs
 
     @property
     def trig_out_cnt(self):
+        """Count of available trigger outputs.
+
+        Returns:
+            int: count of trigger outputs
+        """
         try:
             return libtiepie.DevTrGetOutputCount(self._dev_handle)
         except OSError as err:
@@ -198,9 +241,25 @@ class Device:
                 raise err
 
     def trig_out_by_id(self, trig_out_id):
+        """Get trigger output index by its ID.
+
+        Args:
+            trig_out_id (str): ID of the trigger output as in :py:attr:`tiepie.triggerOutput.TriggerOutput.TRIGGER_IDS`
+
+        Returns:
+            int: index of the requested trigger output
+        """
         id_int = TriggerOutput.TRIGGER_IDS[trig_out_id]
         return libtiepie.DevTrGetOutputIndexById(self._dev_handle, id_int)
 
 
 def version_to_str(raw_version):
+    """Convert a raw version int in TiePie's format to a nice string.
+
+    Args:
+        raw_version (int): concatenated version numbers as int in TiePie's format
+
+    Returns:
+        str: version number as str
+    """
     return '.'.join([str((raw_version >> (idx * 16)) & 0xffff) for idx in range(3,-1,-1)])
